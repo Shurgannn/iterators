eaf8695720f38d0006e3b29b436d3ced7c0e7cc1
Shurgannn <46814630+Shurgannn@users.noreply.github.com>
2019-06-14 15:07:52 +0300

    Add files via upload
    new version of country2.py

---
+++ country3.py
@@ -0,0 +1,39 @@
+import json
+import hashlib
+from pprint import pprint
+
+
+class Country:
+
+    def __init__(self, file_name):
+        self.path = file_name
+        with open(file_name, encoding='utf8') as read_file:
+            data = json.load(read_file)
+        self.data = data
+        self.counter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        country_name = self.data[self.counter]['name']['common']
+        self.counter += 1
+        wiki_link = 'https://wikipedia.org/wiki/' + country_name
+        wiki_link = wiki_link.replace(' ', '')
+        with open('text.txt', 'a', encoding='utf-8') as fw:
+            fw.write(country_name + ' - ' + wiki_link + '\n')
+        if self.counter == len(self.data):
+            raise StopIteration
+        return wiki_link
+
+
+def country(path):
+    with open(path, encoding='utf8') as read_file:
+        for line in read_file.readlines():
+            result = hashlib.md5(line.encode())
+            x = result.hexdigest()
+            yield x
+
+
+for item in country('C:/My documents/iterators/countries.json'):
+    pprint(item)
